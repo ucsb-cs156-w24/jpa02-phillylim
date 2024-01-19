5440cbaac505a689998f98ce344386720c205337
Philly Lim <68778019+phillylim@users.noreply.github.com>
2024-01-18 17:30:20 -0800

    Update HelloController.java

--- src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Philly Lim!";
+        return "Greetings from phillylim!";
     }
     
 }
